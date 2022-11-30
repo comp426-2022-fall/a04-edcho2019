f9edde2c51c0b527929de5bd2b2c2b46c29eb641
edcho2019 <57445838+edcho2019@users.noreply.github.com>
2022-11-30 08:42:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,42 +5,43 @@ import { roll } from "./lib/roll.js";
 const args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
 
-const app = express();
+const application = express();
 app.use(express.urlencoded({extended: true}));
 
-app.get('/app/', (req, res) => {
+application.get('/app/', (req, res) => {
     res.send('200 OK');
     res.status(200);
 })
 
-app.get('/app/roll/', (req, res) => {
+application.get('/app/roll/', (req, res) => {
     res.send(roll(6, 2, 1));
     res.status(200);
 })
 
-app.post('/app/roll/', (req, res) => {
+application.post('/app/roll/', (req, res) => {
     res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));
     res.status(200);
 })
 
-app.get('/app/roll/:sides/', (req, res) => {
+application.get('/app/roll/:sides/', (req, res) => {
     res.send(roll(parseInt(req.params.sides), 2, 1));
     res.status(200);
 })
 
-app.get('/app/roll/:sides/:dice/', (req, res) => {
+application.get('/app/roll/:sides/:dice/', (req, res) => {
     res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
     res.status(200);
 })
 
-app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
+application.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
     res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
     res.status(200);
 })
 
-app.use((req, res) => {
+// if there is an error
+application.use((req, res) => {
 	res.send("404 NOT FOUND");
 	res.status(404);
 });
 
-app.listen(port);
+application.listen(port);
